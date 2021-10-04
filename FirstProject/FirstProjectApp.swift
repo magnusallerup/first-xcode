ba71e3950ec3709a0e5111a21a063176699c1032
Magnus Reidl Allerup <magnusreidl@gmail.com>
2021-10-04 18:52:31 +0200

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Magnus Reidl Allerup on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
